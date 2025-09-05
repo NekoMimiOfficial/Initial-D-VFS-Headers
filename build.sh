df306af5a7aea3b28e30538ce85cdd89fe734fa7
NekoMimi <nekomimiofficial@proton.me>
2025-09-05 16:28:41 +0300

    quick deb output rename

--- build.sh
+++ build.sh
@@ -4,26 +4,26 @@ VERSION="1.0.2"
 
 mkdir -p ./build/
 mkdir -p ./build/lib/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/DEBIAN/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/bin/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/share/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/share/idss/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/include/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/include/IDSSVfs/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/lib/
-mkdir -p ./build/libIDSSVfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/DEBIAN/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/bin/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/share/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/share/idss/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/include/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/include/IDSSVfs/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/lib/
+mkdir -p ./build/libidssvfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/
 
 rm -rf ./build/lib/*
-rm -rf ./build/libIDSSVfs_${VERSION}_amd64/usr/include/IDSSVfs/*
-rm -rf ./build/libIDSSVfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/*
+rm -rf ./build/libidssvfs_${VERSION}_amd64/usr/include/IDSSVfs/*
+rm -rf ./build/libidssvfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/*
 rm -rf ./objects/*
 
-cp -r ./debian/* ./build/libIDSSVfs_${VERSION}_amd64/DEBIAN/
+cp -r ./debian/* ./build/libidssvfs_${VERSION}_amd64/DEBIAN/
 cmake -L .
 make -j4
-cobc -x ./cobol/app.cbl -o ./build/libIDSSVfs_${VERSION}_amd64/usr/bin/idss-vfs
+cobc -x ./cobol/app.cbl -o ./build/libidssvfs_${VERSION}_amd64/usr/bin/idss-vfs
 lua ./lua/processor.lua
 javac -d ./build/ ./java/Installer.java
 cd build
@@ -31,12 +31,12 @@ jar -cfe ./Installer.jar Installer ./Installer.class
 cd ..
 chmod +x ./build/Installer.jar
 chmod +x ./build/Installer.class
-cp -r -d ./objects/libIDSSVfs.so* ./build/libIDSSVfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/
-cp -r -d ./headers/* ./build/libIDSSVfs_${VERSION}_amd64/usr/include/IDSSVfs/
+cp -r -d ./objects/libidssvfs.so* ./build/libIDSSVfs_${VERSION}_amd64/usr/lib/x86_64-linux-gnu/
+cp -r -d ./headers/* ./build/libidssvfs_${VERSION}_amd64/usr/include/IDSSVfs/
 mkdir -p ./build/lib/src/
 mkdir -p ./build/lib/header/
-cp ./doc.txt ./build/libIDSSVfs_${VERSION}_amd64/usr/share/idss/
-cp -r -d ./objects/libIDSSVfs.so* ./build/lib/src/
+cp ./doc.txt ./build/libidssvfs_${VERSION}_amd64/usr/share/idss/
+cp -r -d ./objects/libidssvfs.so* ./build/lib/src/
 cp -r -d ./headers/* ./build/lib/header/
 
-dpkg-deb --build ./build/libIDSSVfs_${VERSION}_amd64
+dpkg-deb --build ./build/libidssvfs_${VERSION}_amd64
